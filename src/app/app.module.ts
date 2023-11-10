aa341a2c0050ea33849c39086bf811b1ad146b52
SpongekYing <spongek.ying@gmail.com>
2023-11-10 17:08:12 +0800

    10.11.1708

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
-import { HomeComponent } from './components/pages/home/home.component'
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CommonModule,
-    RouterModule.forRoot([])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+    HttpClientModule,
+    ReactiveFormsModule,
+    ]})
 export class AppModule { }
